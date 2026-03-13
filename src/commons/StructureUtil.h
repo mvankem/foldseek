a1d36e030a9884fbc5e8c6485b0c5ff73dc0e7f6
Michel van Kempen <michel.van-kempen@mpinat.mpg.de>
2026-03-13 07:37:02 +0100

    Mask 12st sequences at DVP/FGH positions
    Apply masking of 12st alphabet positions to X when 3DI state is
    D/V/P and 12st state is F/G/H. Masking is performed inside
    split3Di12St so all downstream consumers benefit automatically.

--- src/commons/StructureUtil.h
+++ src/commons/StructureUtil.h
@@ -16,6 +16,16 @@ public:
                 & LocalParameters::DBTYPE_EXTENDED_3DI_12ST) != 0;
     }
 
+    // Mask 12st character sequences where 3DI is D/V/P and 12st is F/G/H
+    static inline void mask12StByDVP(char *seq3Di, char *seq12St, size_t len) {
+        for (size_t i = 0; i < len; i++) {
+            if ((seq3Di[i] == 'D' || seq3Di[i] == 'V' || seq3Di[i] == 'P') &&
+                (seq12St[i] == 'F' || seq12St[i] == 'G' || seq12St[i] == 'H')) {
+                seq12St[i] = 'X';
+            }
+        }
+    }
+
     static inline void split3Di12St(const char *src, size_t len,
                                      std::vector<char> &seq3di,
                                      std::vector<char> &seq12st,
@@ -34,6 +44,7 @@ public:
             seq3di[i] = subMat3Di.num2aa[state3di];
             seq12st[i] = subMat12St.num2aa[state12st];
         }
+        mask12StByDVP(seq3di.data(), seq12st.data(), len);
     }
 
     static std::string getIndexWithSuffix(std::string db, const std::string &suffix) {
